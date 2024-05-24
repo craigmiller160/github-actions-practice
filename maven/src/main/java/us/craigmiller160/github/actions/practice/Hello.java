6194c9d3b452141cb3541694a738062a2c827cca
Craig Miller <craigmiller160@gmail.com>
2024-05-24 18:12:44 -0400

    Working on maven test project

---
+++ maven/src/main/java/us/craigmiller160/github/actions/practice/Hello.java
@@ -0,0 +1,7 @@
+package us.craigmiller160.github.actions.practice;
+
+public class Hello {
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
---
+++ maven/src/test/java/us/craigmiller160/github/actions/practice/HelloTest.java
@@ -0,0 +1,4 @@
+package us.craigmiller160.github.actions.practice;
+
+public class HelloTest {
+}

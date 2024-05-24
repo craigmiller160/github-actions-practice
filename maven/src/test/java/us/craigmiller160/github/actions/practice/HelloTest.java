8f50ebab8633ac362a5e9f5efd37398ac17ecb42
Craig Miller <craigmiller160@gmail.com>
2024-05-24 18:14:33 -0400

    Working on maven tests

--- maven/src/test/java/us/craigmiller160/github/actions/practice/HelloTest.java
+++ maven/src/test/java/us/craigmiller160/github/actions/practice/HelloTest.java
@@ -1,4 +1,14 @@
 package us.craigmiller160.github.actions.practice;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class HelloTest {
+
+    @Test
+    public void test_sayHello() {
+        final var hello = new Hello();
+        assertEquals("Hello World", hello.sayHello());
+    }
 }
